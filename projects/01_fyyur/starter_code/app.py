b0591657ff325cafd0bb73c689365b8502a65d00
Joshua Tice <joshtice@gmail.com>
2019-10-06 11:01:17 -0500

    Add ability to insert new artist
    Why: Project requires the ability to add a new artist and persit
    in the database
    
    How: Update appropriate controller to gather form data, create
    Artist object, and then persist to database
    
    On branch fyyur
    Changes to be committed:
    modified:   app.py

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -263,14 +263,29 @@ def create_artist_form():
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
   # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  # DONE: insert form data as a new Venue record in the db, instead
+  # DONE: modify data to be the data object returned from db insertion
+  new_artist = Artist(name=request.form.get('name'))
+  new_artist.city = City.get_unique(
+    city=request.form.get('city'),
+    state=request.form.get('state')
+  )
+  new_artist.phone = request.form.get('phone')
+  new_artist.genres = [
+    Genre.get_unique(name=genre) for genre in request.form.getlist('genres')
+  ]
+  new_artist.facebook_link = request.form.get('facebook_link')
 
   # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
+  try:
+    db.session.add(new_artist)
+    db.session.commit()
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  except:
+    flash('An error occurred. Artist ' + new_artist.name + ' could not be listed.')
   return render_template('pages/home.html')
+
 
 #  ----------------------------------------------------------------
 #  Shows
